2bd35dd3718d73965c022d6af0bf5668512b437b
tigerswim <danhoeller@gmail.com>
2025-10-29 18:23:13 -0400

    chore: Remove duplicate nested RacePrep/raceprep directory

--- RacePrep/raceprep/src/components/training/index.ts
+++
@@ -1,2 +0,0 @@
-export { TrainingCalendar } from './TrainingCalendar';
-export { WorkoutDetailModal } from './WorkoutDetailModal';
--- RacePrep/raceprep/src/screens/Dashboard/DashboardScreen.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { WebDashboard } from '../../components/WebDashboard';
-
-export const DashboardScreen: React.FC = () => {
-  // Use web-optimized dashboard for both web and mobile
-  return <WebDashboard />;
-};
